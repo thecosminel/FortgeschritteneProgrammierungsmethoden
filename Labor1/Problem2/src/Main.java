dd00fc2971bb01eabf162fef34257f31b5e8b20a
George <thecosminel@gmail.com>
2023-10-07 23:41:27 +0300

    Problem2: Losung1.java Losung1Test.java

---
+++ Labor1/Problem2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int [] numbers = {1, 3, 6, 9, 2, 5};
+        int result;
+        // Losung 1
+        result = Losung1.solve(numbers);
+        System.out.print("Problem 2.1: ");
+        System.out.println(result);
+    }
+}
